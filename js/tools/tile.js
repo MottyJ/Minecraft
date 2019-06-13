862e1c89726520d29ae0edea6bcbcc823e4c3797
Elroi Luria <elroi.luria@gmail.com>
2019-06-13 22:02:17 +0300

    add setEmptyState() to Tile

--- js/tools/tile.js
+++ js/tools/tile.js
@@ -1,17 +1,22 @@
 class Tile {
-    constructor(type, isEmpty){
-        // basic tile properties setup
-        this.type = type || ""
-        this.isEmpty = isEmpty 
+  constructor(type, isEmpty) {
+    // basic tile properties setup
+    this.type = type || "";
+    this.isEmpty = isEmpty;
+  }
+  // tile functionality
+  getEmptyState() {
+    return this.isEmpty;
+  }
+  setEmptyState(flag) {
+    if (typeof flag == "boolean") {
+      this.isEmpty = flag;
     }
-    // tile functionality
-    getEmptyState(){
-        return this.isEmpty
-    }
-    getType(){
-        return this.type
-    }
-    setType(newType){
-        this.type = newType
-    }
+  }
+  getType() {
+    return this.type;
+  }
+  setType(newType) {
+    this.type = newType;
+  }
 }
